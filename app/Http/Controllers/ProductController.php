fbefea7b99500ecc1e3df726976c3640863e684e
cisse410 <dramisse90@gmail.com>
2024-03-05 01:51:05 +0000

    intial commit and basic crud operations

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $product = Product::create([
+            'name' => 'Product 3',
+            'slug' => 'product-third',
+            'description' => 'Description of product 3',
+            'price' => 10000
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string'],
+            'slug' => ['required', 'string'],
+            'description' => ['required', 'string'],
+            'price' => ['required', 'integer'],
+        ]);
+        return Product::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return Product::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $product = Product::find($id);
+        $product->update($request->all());
+        return $product;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        return Product::destroy($id);
+    }
+
+    /**
+     * Search for products by name
+     */
+
+    public function search($name)
+    {
+        return Product::where('name', 'like', '%' . $name . '%')->get();
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+
+// Route::resource('/products', ProductController::class);
+// public routes
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/produts/{id}', [ProductController::class, 'destroy']);
+});
